ac2aaafb6517c9a8c6cdd77fbf34663c82bbbe29
pierrekatuhe <36304005+pierrekatuhe@users.noreply.github.com>
2022-05-17 22:55:46 +0800

    Update Read CSV file

--- rabbitmq/read_csv.py
+++ rabbitmq/read_csv.py
@@ -1,16 +1,30 @@
-import pandas as pd
+import os
 import csv
+import pandas as pd
 
 index_row = 0
+last_row = 0
 result_array = []
 
-with open('C:\\Advantech\WISE750\\feature\\ch0\\DATA_20220516203320_0.csv', 'r') as fd:
-    reader=csv.reader(fd)
-    for row in reader:
-        index_row += 1
-        if index_row < 4:
-            continue
-        result_array.append(row)
+# Change this according to your folder path
+path = "E:\\Documents\\National Pingtung University\\Advanced Information Management\\Sensor Data"
 
-print(pd.DataFrame(result_array))
-# print (df)
+try:
+    while True:
+        # Open every file in the folder (Make sure only 1 file exists)
+        for filename in os.listdir(path):
+            with open(os.path.join(path, filename), 'r') as f: # open in readonly mode
+                reader = csv.reader(f)
+                for row in reader:
+                    index_row += 1
+                    if index_row <= 3 or index_row <= last_row:
+                        continue
+                    else:
+                        result_array.append(row)
+
+        last_row = index_row
+        index_row = 0
+
+except KeyboardInterrupt:
+    print("Interrupted")
+    print(pd.DataFrame(result_array))

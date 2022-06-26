fc283fa2859da909f5adf9667f9e94cbb5e77012
taweelaps <taweelap.suwat@gmail.com>
2022-06-26 09:02:27 +0800

    Update predict.py

--- Closet_Sensor/predict.py
+++ Closet_Sensor/predict.py
@@ -1,54 +1,73 @@
 from datetime import datetime
+from datetime import timedelta
 import mysql.connector
+import pandas as pd
 from mysql.connector import Error
 import pickle
 import numpy as np
+#import pika
 
 with open('./model_SVC' , 'rb') as f:
     SVC = pickle.load(f)
-
+#print(datetime.now())
+#print(datetime.now() - timedelta(hours=8))
+rightnow = datetime.now() - timedelta(hours=8, seconds=0.5) #datetime.datetime.now(datetime.timezone.utc)
+#print(rightnow)
+result = []
+result_array = []
+index_row = 0
+last_row = 0
 try:
     while True:
         connection = mysql.connector.connect(
-            host="203.145.218.196",
+            host="203.145.218.196", 
             user="user",
             password="user",
             database="mydb"
         )
-        #index_row = 0
-        #last_row = 0
-        #result_array = []
-        rightnow = datetime.now()
-        timestamp_new_all =[]
-        peaktopeak_value_all =[]
-        label_all=[]
-
+        
         data = connection.cursor()
-        data.execute("SELECT * FROM tb_sensor WHERE sensor_id = '3' AND timestamp >= '" + str(rightnow) + "'")
+        data.execute("SELECT * FROM tb_sensor WHERE sensor_id = '3' AND timestamp >= '" + str(rightnow) + "'") 
         myresult = data.fetchall()
 
-        for x in myresult:
-            #print(x)
-            timestamp_new  = x[0]
-            timestamp_new_all.append(timestamp_new)
-            peaktopeak_value = x[5]
-            peaktopeak_value_all.append(peaktopeak_value)
-            
-            peaktopeak_reshape = np.array(x[5]).reshape(-1, 1)
-            #peaktopeak = np.array(df['peaktopeak']).reshape(-1, 1)
-            
-            # check prediction
-            label = SVC.predict(peaktopeak_reshape) 
-            label_all.append(label)
+        for row in myresult:
+            index_row += 1
+            if index_row <= last_row:
+                continue
+            else:
+                timestamp_new  = row[0]
+                peaktopeak_value = row[5]
+
+                peaktopeak_reshape = np.array(row[5]).reshape(-1, 1)
+                    
+                # check prediction
+                label = SVC.predict(peaktopeak_reshape) 
                 
-            #print (x)
-            mycursor = connection.cursor()
-            sql = "INSERT INTO tb_closet (timestamp_new, peaktopeak_value, label) VALUES ('" + str(timestamp_new) + "', " + str(peaktopeak_value) + ", " + (','.join(label)) + ")"
-            mycursor.execute(sql)
-            connection.commit()
+                mydb = mysql.connector.connect(
+                    host="203.145.218.196", 
+                    user="user",
+                    password="user",
+                    database="mydb"
+                )
+                mycursor = mydb.cursor()
+                sql = "INSERT INTO tb_closet (timestamp_new, peaktopeak_value, label) VALUES ('" + str(timestamp_new) + "', '" + str(peaktopeak_value) + "', '"+ str(*label) + "')"
+
+                mycursor.execute(sql)
+                mydb.commit()
+
+                msg = str(timestamp_new) + ", " + str(peaktopeak_value) + ", "+ str(*label)
+                result.append(msg)
+                result_array = np.array(result)
+                print(' [x]')
+
+        rightnow = timestamp_new
+
+        last_row = index_row
+        index_row = 0
 
 
 except KeyboardInterrupt:
     print("Interrupted")
     #print(pd.DataFrame(result_array))
-    #print(result_array[0])
+    print(result_array)
+    print("Total Record: "+str(len(result_array)))

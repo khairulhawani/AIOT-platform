f34e951cee5aff07bbd8bd3b543e8d0d76a0f7b8
taweelaps <taweelap.suwat@gmail.com>
2022-06-26 08:56:39 +0800

    Update predict data and remove receive, send

--- Closet_Sensor/predict.py
+++ Closet_Sensor/predict.py
@@ -4,10 +4,20 @@ import mysql.connector
 from mysql.connector import Error
 import pickle
 import numpy as np
+import os
+import csv
+import pandas as pd
+import pika
 
 with open('./model_SVC' , 'rb') as f:
     SVC = pickle.load(f)
-
+print(datetime.now())
+print(datetime.now() - timedelta(hours=8))
+rightnow = datetime.now() - timedelta(hours=8, seconds=2) #datetime.datetime.now(datetime.timezone.utc)
+print(rightnow)
+result_array = []
+index_row = 0
+last_row = 0
 try:
     while True:
         connection = mysql.connector.connect(
@@ -16,17 +26,9 @@ try:
             password="user",
             database="mydb"
         )
-        rightnow = datetime.now() - timedelta(hours=8) #datetime.datetime.now(datetime.timezone.utc)
-        #rightnow = '2022-06-25 20:28:37'
-        index_row = 0
-        last_row = 0
-        #result_array = []
-        #timestamp_new_all =[]
-        #peaktopeak_value_all =[]
-        #label_all=[]
-
+        
         data = connection.cursor()
-        data.execute("SELECT * FROM tb_sensor WHERE sensor_id = '3' AND timestamp >= '" + str(rightnow) + "'")
+        data.execute("SELECT * FROM tb_sensor WHERE sensor_id = '3' AND timestamp >= '" + str(rightnow) + "'") #  
         myresult = data.fetchall()
 
         for x in myresult:
@@ -34,23 +36,26 @@ try:
             if index_row <= last_row:
                 continue
             else:
-                #print(x)
                 timestamp_new  = x[0]
-                #timestamp_new_all.append(timestamp_new)
                 peaktopeak_value = x[5]
-                #peaktopeak_value_all.append(peaktopeak_value)
 
                 peaktopeak_reshape = np.array(x[5]).reshape(-1, 1)
-                
+                    
                 # check prediction
                 label = SVC.predict(peaktopeak_reshape) 
-                #label_all.append(label)
-                    
-                print (timestamp_new)
-                mycursor = connection.cursor()
-                sql = "INSERT INTO tb_closet (timestamp_new, peaktopeak_value, label) VALUES ('" + str(timestamp_new) + "', " + str(peaktopeak_value) + ", " + (','.join(label)) + ")"
-                mycursor.execute(sql)
-                connection.commit()       
+                
+                connection = pika.BlockingConnection(
+                    pika.ConnectionParameters(host='203.145.218.196')) # 203.145.218.196
+                channel = connection.channel()
+
+                msg = str(timestamp_new) + ", " + str(peaktopeak_value) + ", "+ str(*label)
+                result_array.append(msg)
+
+                channel.queue_declare(queue='sensor_closet')
+                channel.basic_publish(exchange='', routing_key='sensor_closet', body=(msg).encode('UTF-8'))
+                print(" [x] Sent")
+                connection.close()
+        rightnow = timestamp_new
 
         last_row = index_row
         index_row = 0
@@ -58,5 +63,5 @@ try:
 
 except KeyboardInterrupt:
     print("Interrupted")
-    #print(pd.DataFrame(result_array))
-    #print(result_array[0])
+    print(pd.DataFrame(result_array))
+    print(result_array[0])
